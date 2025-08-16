6859a2c90581afc4d88c8a4fd517c41fbaa2fa73
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2025-08-16 22:04:22 +0100

    Converted to typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import categoryButtons from "./categoryButtons";
+import natureImageData from "./data";
+
+function init() {
+    console.log("Initializing the application...");
+    
+    const categories = Object.keys(natureImageData);
+    console.log("Available categories:", categories);
+    
+    categoryButtons(["All", ...categories]);
+}
+
+
+init();
+
+console.log("This is the main entry point of the application.");
+
